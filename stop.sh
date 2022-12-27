087f9cc4743e7827c5d57aaa678889ababfd2ae0
EricaMaurizi <maurizi.erica@gmail.com>
2022-12-27 15:16:07 +0000

    V.1.0

--- stop.sh
+++ stop.sh
@@ -3,5 +3,5 @@
 docker compose --verbose down
 docker network rm nginx_subnet
 docker rmi $(docker images | grep 'streamlit' | awk '{printf $3}')
-split -b 90M data/u2net.onnx.1 data/u2net.onnx.1_segment-
+split -b 40M data/u2net.onnx.1 data/u2net.onnx.1_segment-
 rm split data/u2net.onnx.1
