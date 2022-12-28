73853d489410ff31899960c8d6d2a915edf8832c
EricaMaurizi <maurizi.erica@gmail.com>
2022-12-28 13:21:22 +0000

    V.1 FIX

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@
 cat data/u2net.onnx_segment-* > data/u2net.onnx
 rm data/u2net.onnx_segment-*
 
+# Certificate copy
+cp $(readlink -f /etc/letsencrypt/live/ericamartina.live/fullchain.pem) ./nginx/fullchain.pem
+cp $(readlink -f /etc/letsencrypt/live/ericamartina.live/privkey.pem) ./nginx/privkey.pem
+
 chmod +x ./stop.sh
 docker network create --subnet=10.5.0.0/16 nginx_subnet
 docker compose --verbose up -d
--- stop.sh
+++ stop.sh
@@ -5,3 +5,5 @@ docker network rm nginx_subnet
 docker image remove $(docker images | grep 'streamlit' | awk '{printf $3}')
 split -b 47M data/u2net.onnx data/u2net.onnx_segment-
 rm data/u2net.onnx
+rm ./nginx/privkey.pem
+rm ./nginx/fullchain.pem
